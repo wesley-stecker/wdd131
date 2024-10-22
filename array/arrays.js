3e8f1b922645c853d11f7a400d88fe95705f1afb
wesley-stecker <Spaceboy623@gmail.com>
2024-10-22 14:28:16 -0600

    working on stuff

---
+++ array/arrays.js
@@ -0,0 +1,7 @@
+//  arrays.js
+const steps = ["one", "two", "three"];
+const listTemplate(step) {
+  return //the html string made from step
+}
+const stepsHtml = // use map to convert the list from strings to HTML
+document.querySelector("#myList").innerHTML = // set the innerHTML
