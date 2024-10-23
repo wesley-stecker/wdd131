25937dc58328c7f3b32f4245989f08f673022b41
wesley-stecker <Spaceboy623@gmail.com>
2024-10-23 11:44:20 -0600

    aaa

--- array/arrays.js
+++ array/arrays.js
@@ -1,7 +1,7 @@
-//  arrays.js
+// example 1
 const steps = ["one", "two", "three"];
 const listTemplate(step) {
-  return //the html string made from step
+  return `<li>${step}</li>`;
 }
-const stepsHtml = // use map to convert the list from strings to HTML
-document.querySelector("#myList").innerHTML = // set the innerHTML
+const stepsHtml = steps.map(listTemplate);
+document.querySelector("#myList").innerHTML = stepsHtml.join();
